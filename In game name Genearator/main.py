06d86dfeac94f0be41b9037d48ad4f702faab150
DeveloperLevin <21f26.levin@sjec.ac.in>
2024-08-24 06:55:09 +0530

    basic ui

--- In game name Genearator/main.py
+++ In game name Genearator/main.py
@@ -1,10 +1,37 @@
-import tkinter as tk
-from langchain.llms import OpenAI
+import customtkinter as ctk
+from dotenv import load_dotenv
+# from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from lanchain.chains import SequentialChain
+from langchain.chains import SequentialChain
+import os
 
-root = tk()
-root.title()
+#load enviornment variables from .env file
+load_dotenv()
+
+#access enviornment variables
+api_key = os.getenv('OPENAI_KEY')
+
+# application configuration
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+root = ctk.CTk()
+root.title("IGN Generator")
+root.geometry("400x300")
+
+#setup basic UI
+title_label = ctk.CTkLabel(root, text="In-Game Name Generator", text_color="blue", font=("Helvetica", 20, "bold"))
+title_label.grid(row=0, column=1, pady=20)
+
+label_option = ctk.CTkLabel(root, text="Select Genre:")
+label_option.grid(row=1, column=0, padx=20, pady=10)
+
+#dropdown box
+options = ["Greek", "Japanese", "Latin", "Indian", "English", "French", "Chinese"]
+dropdown = ctk.CTkComboBox(root, values=options)
+dropdown.grid(row=1, column=1, columnspan=2, pady=10)
+
+output_label = ctk.CTkLabel(root, text="")
+output_label.grid(row=2, column=0, columnspan=3, pady=10)
 
 if __name__ == '__main__':
-    pass
+    root.mainloop()
