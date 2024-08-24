aaee77062a9f6102dc9191aaaddfb0913a714fb7
DeveloperLevin <21f26.levin@sjec.ac.in>
2024-08-24 05:56:28 +0530

    starter files

---
+++ In game name Genearator/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from lanchain.chains import SequentialChain
+
+root = tk()
+root.title()
+
+if __name__ == '__main__':
+    pass
