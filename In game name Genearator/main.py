d81ab5881ff060591ae38f4912953bcbfd6d81e5
DeveloperLevin <21f26.levin@sjec.ac.in>
2024-08-25 09:20:52 +0530

    more ui changes

--- In game name Genearator/main.py
+++ In game name Genearator/main.py
@@ -15,7 +15,7 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 root = ctk.CTk()
 root.title("IGN Generator")
-root.geometry("500x300")
+root.geometry("450x300")
 
 def generate_output():
     #retrieve input values from user
@@ -50,20 +50,17 @@ def generate_output():
     output_name_label.configure(text=f"{username}", font=("Helvetica", 16, "bold"))
     output_desc_label.configure(text=f"{description}")
 
-    print({"name": username, "desc": description})
-
-
 #setup basic UI
 title_label = ctk.CTkLabel(root, text="IGN Generator", text_color="blue", font=("Helvetica", 20, "bold"))
-title_label.grid(row=0, column=1, pady=20)
+title_label.grid(row=0, column=0, columnspan=3, pady=20)
 
 label_option = ctk.CTkLabel(root, text="Select Genre:")
-label_option.grid(row=1, column=0, padx=10, pady=10)
+label_option.grid(row=1, column=0, padx=20, pady=10)
 
 #dropdown box
 options = ["Greek", "Japanese", "Latin", "Indian", "English", "French", "Chinese"]
 dropdown = ctk.CTkComboBox(root, values=options)
-dropdown.grid(row=1, column=1, pady=10)
+dropdown.grid(row=1, column=1, padx=10, pady=10)
 
 generate_button = ctk.CTkButton(root, text="Generate", command=generate_output)
 generate_button.grid(row=1, column=2, pady=10)
